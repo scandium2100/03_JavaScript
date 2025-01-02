3b9162bac21c743d6d47640e33cd2320a46c03c5
scandium2100 <khk6457@naver.com>
2025-01-02 17:38:57 +0900

    2025/01/02 JS 기초 변수 배움

--- JS/02_변수와자료형..js
+++ JS/02_변수와자료형..js
@@ -135,3 +135,64 @@ function scopeTest2(){
     }
     console.log("if 후 이름 : ", name); /* 홍길동 */
 }
+
+// 자료형 확인
+function typeCheck(){
+    /* typeof 연산자 : 변수/값의 자료형을 확인하는 연산자 */
+    /* undefined : 정의되지 않음 == 변수에 값 대입 X */
+
+    let undef;
+    console.log("undef : ", undef, typeof undef);
+    
+    // 문자열(String) : "", '' 형태로 작성된 리터럴(값)
+    const name = "성기훈"
+    const phone = '01012341234';
+    console.log("name : ", name, typeof name);
+    console.log("phone number : ", phone, typeof phone);
+
+    // number(숫자) : 정수나 실수 형태으 리터럴(값)
+    const age = 26;
+    const height = 178;
+    const sight = -2;
+    console.log("age : ", age, typeof age);
+    console.log("height : ", height, typeof height);
+    console.log("sight : ", sight, typeof sight);
+
+    // boolean(논리값) : true, false
+    const bool1 = true;
+    const bool2 = false;
+    console.log("bool1 : ", bool1, typeof bool1);
+    console.log("bool2 : ", bool2, typeof bool2);
+
+    // object(객체) : 값을 여러개 저장 할 수 있는 형태
+    //  1) 배열(Array) : 변수의 묶음, 
+    //                   묶인 변수에 0부터 1씩 증가하는 숫자를 부여(index)
+    const numbers = [1, 2, 3];
+    console.log("numbers : ", numbers, typeof numbers);
+    // 배열 내 요소(저장된 값) 하나씩 꺼내기
+    console.log("numbers[0] : ", numbers[0], typeof numbers[0]);
+    console.log("numbers[1] : ", numbers[1], typeof numbers[1]);
+    console.log("numbers[2] : ", numbers[2], typeof numbers[2]);
+
+    // JS 객체 { k:v, k:v, k:v }
+    // k(key) == 값을 구분하는 이름(변수명과 비슷)
+    // v(value) == 값 자체(변수에 대입되는 값)
+    const user = { id:"user01",
+                   pw:"pass01", 
+                   point:13000 }
+    console.log("user : ", user, typeof user); //object type
+    console.log("user['id'] : ", user['id'], typeof user['id']);
+    console.log("user['pw'] : ", user['pw'], typeof user['pw']);
+    console.log("user['point'] : ", user['point'], typeof user['point']);
+    
+    // function(함수) : 함수도 변수에 대입 가능!!!
+    // sumFn == 함수명
+    const sumFn = function(a, b){
+        return a + b;
+    }
+    console.log("sumFn : ", sumFn, typeof sumFn);
+    //  sumFn 호출
+    console.log(sumFn(111, 987), typeof sumFn);
+    console.log(sumFn(1, 9), typeof sumFn);
+    console.log(sumFn(-182, 100), typeof sumFn);
+}
